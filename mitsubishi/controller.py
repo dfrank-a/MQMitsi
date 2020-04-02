6c02f50f8e19f21f39bf1ae4d5b9e431764fe661
Domenic Ariaudo <d.ariaudo@dickblick.com>
2020-04-01 23:01:37 -0400

    [threaded] fix?

--- mitsubishi/controller.py
+++ mitsubishi/controller.py
@@ -78,7 +78,7 @@ class HeatPumpController:
                 self.device_queue.task_done()
             except QueueEmpty:
                 pass
-            sleep(0)
+            sleep(0.1)
 
     def read_device_stream(self):
         while True:
