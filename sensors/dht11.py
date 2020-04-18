8a60933f104ff89be6f2a54f422f8b3f50188781
Domenic Ariaudo <d.ariaudo@dickblick.com>
2020-04-17 21:53:35 -0400

    [dh11-sensor] fix

--- sensors/dht11.py
+++ sensors/dht11.py
@@ -31,7 +31,7 @@ def on_mqtt_disconnect(topic_prefix):
 
 
 def __main__(
-    broker="127.0.0.1", broker_port=1883, topic_prefix="growroom/sensors/dh11"
+    broker="127.0.0.1", broker_port=1883, topic_prefix="grow_room/sensors/dh11"
 ):
     client = mqtt.Client(protocol=mqtt.MQTTv31)
     client.on_connect = on_mqtt_connect(topic_prefix)
@@ -51,12 +51,12 @@ def __main__(
                 temperature_c = cur_temperature_c
                 temperature_f = round(cur_temperature_c * (9 / 5) + 32, 2)
                 logger.info(f"Temp: {temperature_f:.1f} F / {temperature_c:.1f} C")
-                client.publish(f"topic_prefix/temperature", temperature_c, qos=1)
+                client.publish(f"{topic_prefix}/temperature", temperature_c, qos=1)
 
             if cur_humidity != humidity:
                 humidity = cur_humidity
                 logger.info(f"Humidity: {humidity}%")
-                client.publish(f"topic_prefix/humidity", humidity, qos=1)
+                client.publish(f"{topic_prefix}/humidity", humidity, qos=1)
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
             print(error.args[0])
