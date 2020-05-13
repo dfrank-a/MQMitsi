973aae0b7762b6ccee8fa208d154a08c06ede9ec
Domenic Ariaudo <d.ariaudo@dickblick.com>
2020-05-12 22:09:03 -0400

    [dh11-sensor] daemon readiness

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="mitsubishi",
+    name="growroom",
     version="1.0.0",
-    description="Mitsubishi heat pump controller",
-    install_requires=[],
+    description="Grow room controller",
+    install_requires=[
+        "adafruit-circuitpython-dht==3.4.1",
+        "paho-mqtt==1.4.0",
+        "pyserial==3.4"
+    ],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'Environment :: Console'
     ],
-    packages=["mitsubishi"],
+    packages=find_packages(),
     entry_points={
-        'console_scripts': []
+        'console_scripts': ["growroom=growroom:main"]
     }
 )
